ae6a4e95f9709690b2dd4e6a34deeced5372f63c
luizaalanis <luizalanisantos@gmail.com>
2023-03-05 19:17:47 -0300

    First commit

---
+++ src/main/java/com/client/ws/spring/SpringWsApplication.java
@@ -0,0 +1,13 @@
+package com.client.ws.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWsApplication.class, args);
+    }
+
+}
